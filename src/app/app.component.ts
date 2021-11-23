6415f621964f45ade6f24ce8ddd087bad23e5926
PantiraR818 <pantiraploy818@gmail.com>
2021-11-23 16:56:50 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BS2';
+  n1: number =5;
+  n2: number =2;
+  picName:string = "https://cdn.pixabay.com/photo/2021/05/23/00/21/woman-6274879_960_720.png";
+
+  // Func
+  hi(name:string){
+    alert("Hi Hi!!"+ name);
+  }
 }
+
